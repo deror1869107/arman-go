6ceec5c770a431249fcc7aba1cf756e3891e5e1e
Yuan-Yao Sung <sungyuanyao@gmail.com>
2016-08-13 16:29:38 +0800

    English JSON

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -8,16 +8,16 @@ class Plan < ActiveRecord::Base
     if self.locations.size > 0
       start = [self.start_lat, self.start_lng]
       ed = [self.end_lat, self.end_lng]
-      locations = self.locations.map { |l| [l.lat, l.lng, l.ch_name] }
+      locations = self.locations.map { |l| [l.lat, l.lng, l.en_name] }
       waypoints = locations.map { |l| "#{l[0]},#{l[1]}"}.join("|")
       uri = URI("https://maps.googleapis.com/maps/api/directions/json?origin=#{start.first},#{start.last}&destination=#{ed.first},#{ed.last}&waypoints=optimize:true|#{waypoints}&key=#{ENV['GOOGLE_MAP_API_KEY']}")
       j = JSON.parse(Net::HTTP.get(uri))
       order = j["routes"][0]["waypoint_order"]
-      res = ["臺北車站"]
+      res = ["Taipei Railway Station"]
       order.each do |index|
         res << locations[index].last
       end
-      res << "臺北車站"
+      res << "Taipei Railway Station"
       return res
     end
   end
