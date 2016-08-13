7cd64db891465a3576ef6b027147e8d5b77021f5
Yuan-Yao Sung <sungyuanyao@gmail.com>
2016-08-13 15:40:10 +0800

    Sign Out

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,3 +5,7 @@
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ ArmanPlanner::Application.routes.draw do
     :registrations => "users/registrations",
     :omniauth_callbacks => "users/omniauth_callbacks"
   }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
